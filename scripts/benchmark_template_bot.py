e8666b2662796dbec6f3c031e20969694adcebac
InfiniteQuery <43260133+thebuilder6@users.noreply.github.com>
2025-01-07 13:48:55 -0700

    Update benchmark_template_bot.py

--- scripts/benchmark_template_bot.py
+++ scripts/benchmark_template_bot.py
@@ -1,28 +1,41 @@
-import asyncio
 
-from forecasting_tools import Benchmarker, BenchmarkForBot, TemplateBot
+from forecasting_tools import (
+    MetaculusApi,
+    TemplateBot,
+)
+
+question = MetaculusApi.get_question_by_post_id(11245)  # US 2024 Election
+question = MetaculusApi.get_question_by_url(
+    "https://www.metaculus.com/questions/11245/..."
+)
+templatebot = TemplateBot()
 
 
 async def benchmark_template_bot() -> None:
-    # Run benchmark on multiple bots
-    bots = [TemplateBot()]  # Add your custom bots here
-    benchmarker = Benchmarker(
-        forecast_bots=bots,
-        number_of_questions_to_use=2,  # Recommended 100+ for meaningful results
-        file_path_to_save_reports="benchmarks/",
-    )
-    benchmarks: list[BenchmarkForBot] = await benchmarker.run_benchmark()
-
-    # View results
-    for benchmark in benchmarks:
-        print(f"Bot: {benchmark.name}")
-        print(
-            f"Score: {benchmark.average_inverse_expected_log_score}"
-        )  # Lower is better
-        print(f"Num Forecasts: {len(benchmark.forecast_reports)}")
-        print(f"Time: {benchmark.time_taken_in_minutes}min")
-        print(f"Cost: ${benchmark.total_cost}")
+    report = await templatebot.forecast_questions([question])
+    print(report.explanation)
 
 
-if __name__ == "__main__":
-    asyncio.run(benchmark_template_bot())
+# async def benchmark_template_bot() -> None:
+#     # Run benchmark on multiple bots
+#     bots = [] # Add your custom bots here
+#     benchmarker = Benchmarker(
+#         forecast_bots=bots,
+#         number_of_questions_to_use=2,  # Recommended 100+ for meaningful results
+#         file_path_to_save_reports="benchmarks/",
+#     )
+#     benchmarks: list[BenchmarkForBot] = await benchmarker.run_benchmark()
+
+#     # View results
+#     for benchmark in benchmarks:
+#         print(f"Bot: {benchmark.name}")
+#         print(
+#             f"Score: {benchmark.average_inverse_expected_log_score}"
+#         )  # Lower is better
+#         print(f"Num Forecasts: {len(benchmark.forecast_reports)}")
+#         print(f"Time: {benchmark.time_taken_in_minutes}min")
+#         print(f"Cost: ${benchmark.total_cost}")
+
+
+# if __name__ == "__main__":
+#     asyncio.run(benchmark_template_bot())
