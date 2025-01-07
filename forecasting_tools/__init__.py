ffbb1cdf99c3cb813093fa975a6f633defee02cc
InfiniteQuery <43260133+thebuilder6@users.noreply.github.com>
2025-01-07 00:59:32 -0700

    Update __init__.py

--- forecasting_tools/__init__.py
+++ forecasting_tools/__init__.py
@@ -7,6 +7,9 @@ from forecasting_tools.ai_models.claude35sonnet import (
 from forecasting_tools.ai_models.exa_searcher import ExaSearcher as ExaSearcher
 from forecasting_tools.ai_models.gpt4o import Gpt4o as Gpt4o
 from forecasting_tools.ai_models.gpt4ovision import Gpt4oVision as Gpt4oVision
+from forecasting_tools.ai_models.gemini2flashthinking import (
+    Gemini2FlashThinking as Gemini2FlashThinking,
+)
 from forecasting_tools.ai_models.metaculus4o import (
     Gpt4oMetaculusProxy as Gpt4oMetaculusProxy,
 )
